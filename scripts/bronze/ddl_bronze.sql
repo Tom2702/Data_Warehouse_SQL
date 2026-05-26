293768f366faf501e789b34d001d3cfe35f65cad
Tom2702 <161796809+Tom2702@users.noreply.github.com>
2026-05-26 11:54:59 +0700

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -5,6 +5,7 @@ DDL Scripts: Create Bronze Table
 Script's Purpose:
   This script creates tables in the 'bronze' schema, dropping existing tables
   if they already exist.
+=============================================================================
 */
 
 IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL
