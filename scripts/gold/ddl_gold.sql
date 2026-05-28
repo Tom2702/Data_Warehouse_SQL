454770041f6d1e0092321d51e7f75d2d176c36b1
Tom2702 <161796809+Tom2702@users.noreply.github.com>
2026-05-28 11:51:13 +0700

    Update ddl_gold.sql

--- scripts/gold/ddl_gold.sql
+++ scripts/gold/ddl_gold.sql
@@ -23,12 +23,12 @@ GO
 CREATE VIEW gold.dim_customers AS
 SELECT
     ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
-    ci.cst_id                          AS customer_id,
-    ci.cst_key                         AS customer_number,
-    ci.cst_firstname                   AS first_name,
-    ci.cst_lastname                    AS last_name,
-    la.cntry                           AS country,
-    ci.cst_marital_status              AS marital_status,
+    ci.cst_id                           AS customer_id,
+    ci.cst_key                          AS customer_number,
+    ci.cst_firstname                    AS first_name,
+    ci.cst_lastname                     AS last_name,
+    la.cntry                            AS country,
+    ci.cst_marital_status               AS marital_status,
     CASE 
         WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr -- CRM is the primary source for gender
         ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
